fad5c098fcff798e4003a9d5e5c16c591f1df05b
giannismanie <43655664+giannismanie@users.noreply.github.com>
2022-09-08 13:09:49 +0300

    Created expenseItem component & styling

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,36 @@ const Dummy_Expenses = [
         amount: 39.99,
         date: new Date('2022-01-11')
     },
+    {
+      id: 'e6',
+      description: 'A pair of shoes',
+      amount: 59.99,
+      date: new Date('2022-06-15')
+  },
+  {
+      id: 'e7',
+      description: 'A pair of shoes',
+      amount: 89.99,
+      date: new Date('2022-07-15')
+  },
+  {
+      id: 'e8',
+      description: 'A pair of trousers',
+      amount: 189.99,
+      date: new Date('2022-06-17')
+  },
+  {
+      id: 'e9',
+      description: 'A book',
+      amount: 9.99,
+      date: new Date('2022-02-10')
+  },
+  {
+      id: 'e10',
+      description: 'A pair of shoes',
+      amount: 39.99,
+      date: new Date('2022-01-11')
+  },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -50,7 +80,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   }
 });
