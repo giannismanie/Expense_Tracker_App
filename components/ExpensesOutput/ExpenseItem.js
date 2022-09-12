90833c19ef44da3e39afb12a95e3e4b1049943ff
giannismanie <43655664+giannismanie@users.noreply.github.com>
2022-09-12 11:57:27 +0300

    Added onPress action on items component

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,9 +1,14 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
 function ExpenseItem({ description, date, amount }) {
-  function expensePressHandler() {}
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate('ManageExpense');
+  }
 
   return (
     <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
