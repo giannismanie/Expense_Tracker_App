7a47f76426721c940fb0ca6eb623a930808b00d6
giannismanie <43655664+giannismanie@users.noreply.github.com>
2022-09-12 11:48:32 +0300

    Added onPress styling on items component

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -3,8 +3,10 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 function ExpenseItem({ description, date, amount }) {
+  function expensePressHandler() {}
+
   return (
-    <Pressable>
+    <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>{description}</Text>
@@ -21,6 +23,9 @@ function ExpenseItem({ description, date, amount }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.7
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
